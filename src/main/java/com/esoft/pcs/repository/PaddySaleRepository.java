520c4b2fe3cd83a3b9011bb66012308e2027ba12
Jaith Kaushalya <jaithkaushalya99@gmail.com>
2021-11-25 01:06:49 +0530

    Adding report service implementations

---
+++ src/main/java/com/esoft/pcs/repository/PaddySaleRepository.java
@@ -0,0 +1,7 @@
+package com.esoft.pcs.repository;
+
+import com.esoft.pcs.models.PaddySale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaddySaleRepository extends JpaRepository<PaddySale, Integer> {
+}
