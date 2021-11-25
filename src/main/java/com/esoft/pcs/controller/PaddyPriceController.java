3b1fb2c6dd50e56ebe6612d6456213dcc0862901
vinodlakshan <vinodlakshan16@gmail.com>
2021-11-25 11:39:20 +0530

    completed Implementing purchase paddy

--- src/main/java/com/esoft/pcs/controller/PaddyPriceController.java
+++ src/main/java/com/esoft/pcs/controller/PaddyPriceController.java
@@ -2,14 +2,18 @@ package com.esoft.pcs.controller;
 
 import com.esoft.pcs.models.PaddyPrice;
 import com.esoft.pcs.service.PaddyPriceService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/paddyPrice")
+@Slf4j
 public class PaddyPriceController
 {
     @Autowired
@@ -19,13 +23,13 @@ public class PaddyPriceController
     @GetMapping("/TodayPaddyPriceGet")
     public ResponseEntity<PaddyPrice> TodayPaddyPriceGet(HttpServletRequest request) throws Exception
     {
-        return paddyPriceService.TodayPaddyPrice();
+        return new ResponseEntity<>(paddyPriceService.TodayPaddyPrice(), HttpStatus.OK);
     }
 
     // Arun
     @PostMapping("/PaddyPriceSave")
     public ResponseEntity<PaddyPrice> PaddyPriceSave(@RequestBody PaddyPrice paddyPrice, HttpServletRequest request) throws Exception
     {
-        return paddyPriceService.PaddyPriceSave(paddyPrice);
+        return new ResponseEntity<>(paddyPriceService.PaddyPriceSave(paddyPrice), HttpStatus.OK);
     }
 }
--- src/main/java/com/esoft/pcs/repository/PaddyPriceRepository.java
+++ src/main/java/com/esoft/pcs/repository/PaddyPriceRepository.java
@@ -3,7 +3,7 @@ package com.esoft.pcs.repository;
 import com.esoft.pcs.models.PaddyPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaddyPriceRepository extends JpaRepository<PaddyPrice, Long>
+public interface PaddyPriceRepository extends JpaRepository<PaddyPrice, Integer>
 {
     PaddyPrice getTopByDate(String date);
 }
