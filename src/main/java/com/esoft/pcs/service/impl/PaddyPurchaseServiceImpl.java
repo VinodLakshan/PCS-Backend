ea2de299de57ac3791c5c0480d9608721b2c6923
rabeez <armrabeez@gmail.com>
2021-12-08 18:05:54 +0530

    fixed API errors

--- src/main/java/com/esoft/pcs/service/impl/PaddyPurchaseServiceImpl.java
+++ src/main/java/com/esoft/pcs/service/impl/PaddyPurchaseServiceImpl.java
@@ -2,18 +2,23 @@ package com.esoft.pcs.service.impl;
 
 import com.esoft.pcs.models.Branch;
 import com.esoft.pcs.models.PaddyPurchase;
+import com.esoft.pcs.repository.BranchRepository;
 import com.esoft.pcs.repository.PaddyRepository;
 import com.esoft.pcs.service.PaddyPurchaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PaddyPurchaseServiceImpl implements PaddyPurchaseService {
 
     @Autowired
     private PaddyRepository paddyRepository;
+
+    @Autowired
+    private BranchRepository branchRepository;
 
 
     @Override
@@ -33,7 +38,9 @@ public class PaddyPurchaseServiceImpl implements PaddyPurchaseService {
     {
         PaddyPurchase pendingPurchase = paddyRepository.findById(paddyPurchase.getId()).orElse(null);
         pendingPurchase.setStatus(paddyPurchase.getStatus());
-
+        Optional<Branch> dbBranch = branchRepository.findById(pendingPurchase.getBranch().getId());
+        dbBranch.get().setStock(dbBranch.get().getStock()+pendingPurchase.getWeight());
+        branchRepository.save(dbBranch.get());
         return paddyRepository.save(pendingPurchase);
     }
 }
--- src/main/java/com/esoft/pcs/service/impl/PaddySellingServiceImpl.java
+++ src/main/java/com/esoft/pcs/service/impl/PaddySellingServiceImpl.java
@@ -2,6 +2,7 @@ package com.esoft.pcs.service.impl;
 
 import com.esoft.pcs.models.*;
 
+import com.esoft.pcs.repository.BranchRepository;
 import com.esoft.pcs.repository.CustomerRepository;
 import com.esoft.pcs.repository.PaddySaleRepository;
 import com.esoft.pcs.repository.PaymentRepository;
@@ -10,12 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PaddySellingServiceImpl implements PaddySellingService {
 
   @Autowired
   private PaddySaleRepository paddySaleRepository;
+
+  @Autowired
+  private BranchRepository branchRepository;
 
   @Autowired
   private CustomerRepository customerRepository;
@@ -45,7 +50,9 @@ public class PaddySellingServiceImpl implements PaddySellingService {
   {
     PaddySale pendingSale = paddySaleRepository.findById(paddySale.getId()).orElse(null);
     pendingSale.setStatus(paddySale.getStatus());
-
+    Optional<Branch> dbBranch = branchRepository.findById(pendingSale.getBranch().getId());
+    dbBranch.get().setStock(dbBranch.get().getStock()-pendingSale.getWeight());
+    branchRepository.save(dbBranch.get());
     return paddySaleRepository.save(pendingSale);
   }
 }
